612f27b0621ba9e1140655096f9821d7cb29b584
birand <birandbosley@gmail.com>
2026-03-01 21:19:43 +0300

    feat: add C++ compatibility via extern 'C' guards
    - Wrapped function declarations in extern 'C' blocks to support C++ integration.
    - Ensures correct name mangling when linking from C++ projects.

--- nova_drop.h
+++ nova_drop.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define NOVA_DROP_STATE_SIZE 5
 
 typedef struct {
@@ -11,5 +15,9 @@ typedef struct {
 
 void nova_init(NovaState *state, uint32_t seed);
 uint32_t nova_drop(NovaState *state);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // NOVA_DROP_H
